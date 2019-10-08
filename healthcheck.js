f5a7b1191ba0acd14217a2ce44fc8ebff74eb5b4
Daniel Rawlins <danrawlins07@gmail.com>
2019-10-08 12:47:41 -0400

    we now have a gitignore and valid 200 response in json

--- healthcheck.js
+++ healthcheck.js
@@ -4,7 +4,16 @@ var lightship = require ('lightship');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('200');
+//  console.log("Request handler random was called.");
+  res.writeHead(200, {"Content-Type": "application/json"});
+  var otherArray = ["Health", "Status"];
+  var otherObject = { Health: "Up", Status: "Working" };
+  var json = JSON.stringify({
+    anObject: otherObject,
+    anArray: otherArray,
+    another: "item"
+  });
+  res.end(json);
 });
 
 const server = app.listen(8080);
